9c1827f0dcd655fe28517f50afbd57ca23943d4f
DejanovicIT42 <73885609+DejanovicIT42@users.noreply.github.com>
2024-10-02 15:03:00 +0200

    Add project files.

---
+++ MemgraphApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MemgraphApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
